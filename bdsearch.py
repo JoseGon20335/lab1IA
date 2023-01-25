b0c67971a84081494112618aad85cb0a128d1629
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2023-01-24 19:10:50 -0600

    Update bdsearch.py

--- bdsearch.py
+++ bdsearch.py
@@ -1,32 +1,14 @@
 from collections import deque
 from abc import ABC, abstractmethod
 
-
-matrixx = [[0,0,0,0,0],
-          [1,0,1,1,0],
-          [0,0,1,0,0],
-          [0,1,0,0,1],
-          [0,0,0,0,0],
-                     ]
-
-def search(start, camino, graph):
-  space = []
-  temp = start
-  for x in camino:
-    lista = graph[temp]
-    for y in lista:
-      if y[0] == x:
-        temp = y[1]
-        space.append(temp)
-  space.pop(-1)
-  return space
-
-
 class busqueda(ABC):
   matrix = None
   graph = None
 
-  def __init__(self, matrix):
+  def __init__(self, matrix, start, end):
+    self.start = start
+    self.end = end
+    self.camino = None
     self.matrix = matrix
     self.graph = self.grafo()
 
@@ -43,21 +25,32 @@ class busqueda(ABC):
         grafo[(par[0], par[1]+1)].append(("O", par))
     return grafo
 
-  @abstractmethod
-  def algoritmo(self):
-    pass
+  def search(self):
+    space = []
+    temp = self.start
+    for x in self.camino:
+      lista = self.graph[temp]
+      for y in lista:
+        if y[0] == x:
+          temp = y[1]
+          self.matrix[temp[0]][temp[1]] = 4
+          space.append(temp)
+    self.matrix[temp[0]][temp[1]] = 3  
+    space.pop(-1)
+    return space
+
 
 class breadth_first(busqueda):
-  def __init__(self):
-    super().__init__(matrixx)
 
   def algoritmo(self):
-    start,end = (0,0),[(4,4), (0,4)]
-    cola = deque([("", start)])
+    cola = deque([("", self.start)])
+    self.matrix[int(self.start[0])][int(self.start[1])] = 2 
     visitado = set()
     while cola:
       camino, current = cola.popleft()
-      if current in end:
+      if current in self.end:
+        self.matrix[current[0]][current[1]] = 3 
+        self.camino = camino
         return camino
       if current in visitado:
         continue
@@ -65,22 +58,22 @@ class breadth_first(busqueda):
       for direction, vecino in self.graph[current]:
         cola.append((camino+direction,vecino))
 
+
 class depth_first(busqueda):
-  def __init__(self):
-    super().__init__(matrixx)
 
   def algoritmo(self):
-    start,end = (0,0),[(4,4), (0,4)]
-    stack = deque([("", start)])
+    stack = deque([("", self.start)])
+    self.matrix[int(self.start[0])][int(self.start[1])] = 2 
     visitado = set()
     while stack:
       camino, current = stack.pop()
-      if current in end:
+      if current in self.end:
+        self.matrix[current[0]][current[1]] = 3 
+        self.camino = camino
         return camino
       if current in visitado:
         continue
       visitado.add(current)
-      for direction, vecino in grafo[current]:
+      for direction, vecino in self.graph[current]:
         stack.append((camino+direction,vecino))
 
-  
