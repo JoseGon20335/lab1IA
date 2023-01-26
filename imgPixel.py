4c1b518de3a75d1a9de2aaed655bfe29c708f978
JoseGonzalez098 <gon20335@uvg.edu.gt>
2023-01-25 18:05:39 -0600

    imgPixel return matrix

--- imgPixel.py
+++ imgPixel.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw
 from numpy import asarray
 import matplotlib.pyplot as plt
+from numpy import array
 
 
 # Regresa un array reducido de pixeles de la imagen
@@ -10,19 +11,31 @@ def imgPixel(img):
     myimg = Image.open(img)
     myimg = myimg.resize((250, 250))
 
-    # pixelart = Image.new('RGB', myimg.size, (255, 255, 255))
-    # pixel = myimg.load()
-
-    # print(myimg)
-    # print(pixel)
-    # print(pixelart)
-    # # numpydata2 = asarray(myimg)
-    # numpydata2 = asarray(pixelart)
-    # return numpydata2
-
     pequeImg = myimg.resize((40, 40), Image.BILINEAR)
-    iSize = (250, 250)
+    iSize = (70, 70)
     pixelart = pequeImg.resize(iSize, Image.NEAREST)
-    pixelart.save("pixelart.png")
-    # Regresa un array de pixeles de la imagen
-    # Recibe como parametro la ruta de la imagen
+    resultado = "pixelart.png"
+    pixelart.save(resultado)
+    im_1 = Image.open(resultado)
+    ar = array(im_1)
+    x = ar.tolist()
+    matrix = []
+    salida = []
+    entrada = 0
+    for i in range(0, len(x), 2):
+        temp = []
+        for j in range(0, len(x), 2):
+            if x[i][j][0] == x[i][j][1] and x[i][j][0] >= 255 or x[i][j][0] >= 250 and x[i][j][1] >= 20 or x[i][j][1] >= 250 and x[i][j][0] >= 20:
+                temp.append(0)
+            elif x[i][j][1] >= 250 and x[i][j][0] <= 20:
+                temp.append(2)
+                salida.append((int(j/2), int(i/2)))
+            elif x[i][j][0] >= 250 and x[i][j][1] <= 20:
+                temp.append(3)
+                entrada = (int(j/2), int(i/2))
+            else:
+                temp.append(1)
+
+        matrix.append(temp)
+
+    return matrix, entrada, salida
--- main.py
+++ main.py
@@ -11,31 +11,20 @@ from imgPixel import imgPixel
 def main():
 
     imagen = "prueba.png"
-    imgPixel(imagen)
-    matrix = []
-    salida = []
-    entrada = 0
-    for y in range(0,len(x),10):
-      temp = []
-      for z in range(0,len(x),10):
-        if x[y][z][0] == x[y][z][1] and x[y][z][0] >= 225:
-          temp.append(0)
-        elif x[y][z][1] >= 250 and x[y][z][0] <= 20:
-          temp.append(0)
-          salida.append((int(z/10),int(y/10)))
-        elif x[y][z][0] >= 250 and x[y][z][1] <= 20:
-          temp.append(0)
-          entrada = (int(z/10),int(y/10))
-        else: 
-          temp.append(1)
-      matrix.append(temp)
+    matrix, entrada, salida = imgPixel(imagen)
+
+    print("Entrada: ", entrada)
+    print("Salida: ", salida)
+    for i in range(len(matrix)):
+        print(matrix[i])
 
     menu = True
     while menu:
         print("------------Menu------------")
         print("1. Graph-Search")
         print("2. Depth-First-Search")
-        print("3. Exit")
+        print("3. Breadth-First-Search")
+        print("4. Exit")
         option = int(input("Select an option: "))
 
         if option == 1:
@@ -43,13 +32,13 @@ def main():
             # Aqui va donde llamamos a la funcion de graph-search
         elif option == 2:
             print("Bredth-First-Search")
-            breadth = breadth_first(matrix,entrada, salida)
+            breadth = breadth_first(matrix, entrada, salida)
             print(breadth.algoritmo())
             print(breadth.search())
             print(breadth.matrix)
-         elif option == 3:
+        elif option == 3:
             print("Bredth-First-Search")
-            depth = depth_first(matrix,entrada, salida)
+            depth = depth_first(matrix, entrada, salida)
             print(depth.algoritmo())
             print(depth.search())
             print(depth.matrix)
