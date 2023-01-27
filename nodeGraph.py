756eb2bc590ad41f47440f086b10b7baa4096691
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2023-01-26 23:28:05 -0600

    Estructura tercera versión a*
    Versión basada en artrículo de internet más apropiada

--- nodeGraph.py
+++ nodeGraph.py
@@ -1,19 +1,3 @@
-
-# Description: Implementación de un grafo de nodos para el algoritmo A*
-
-class Node():
-    
-    def __init__(self, pos = None, parent = None):
-        self.pos = pos
-        self.parent = None
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.pos == other.pos
-
-
 # Heurísticas
 
 # Manhattan
@@ -34,156 +18,218 @@ def Diagonal(x1, y1, x2, y2, D = 1):
         return (dx + dy) * D + (D * 2 - 2 * D) * min(dx, dy)
 
 
+# Description: Implementación de un grafo de nodos para el algoritmo A*
+
+class Node():
+    
+    def __init__(self, pos = None, parent = None):
+        self.pos = pos #posición del nodo
+        self.parent = parent #nodo padre, osea el previo al actual
+        self.g = 0 #costo del camino desde el nodo inicial hasta el actual
+        self.h = 0 #heurística del nodo actual hasta el nodo final
+        self.f = 0 #f = g + h
+
+    def __eq__(self, other):
+        return self.pos == other.pos
+
+
+    def setH(self, end, heuristic):
+        if heuristic == "manhattan":
+            self.h = manhattan(self.pos[0], self.pos[1], end[0], end[1])
+        elif heuristic == "diagonal":
+            self.h = Diagonal(self.pos[0], self.pos[1], end[0], end[1])
+        self.f = self.g + self.h
+
+
+
 # Algoritmo A*
 
-def astar(maze, start, end, heuristic = "manhattan"):
-    node_i = Node(start, None)
-    node_f = Node(end, None)
 
-    _open = []
-    _closed = []
+def astar_3(m, s, es, h = "manhattan"):
+    n_i = Node(s, None)
+    paths = []
 
-    _open.append(node_i)
-    #print("open: ", _open[0].pos , " , ",  _open[0].parent) 
-    print(_open)
-    print(_closed)
-    #print
-    #print("closed: ", _closed[0].pos , " , ",  _closed[0].parent)
+    for e in es:
+        #movimientos posibles: derecha, izquierda, arriba, abajo
+
+        #paso 1: nodo inicial
+
+        #nodo inicial
+        n_i.setH(e, h)
+        n_actual = n_i
 
 
-    while len(_open) > 0:
-        #print("entrando al while")
+        #lista de nodos
+        nodes = []
+        nodes.append(n_i)
 
-        node_c = _open[0]
-        node_c_index = 0
-        for i, item in enumerate(_open):
-            if item.f < node_c.f:
-                node_c = item
-                node_c_index = i
+        #nodo con menor f
+        lowest_f = [n_i.f, n_i.pos]
 
-        _open.pop(node_c_index)
-        _closed.append(node_c)
-
-        #rint(" still alive")
-
-        if node_c == node_f:
-            path = []
-            current = node_c
-            while current is not None:
-                path.append(current.pos)
-                current = current.parent
-            return path[::-1]
+        print(e)
+        print(n_actual.pos)
 
         
-        print(node_c.pos)
-        print(node_f.pos)
+        while n_actual.pos != e:
 
-        sons = []
+            #nodo actual y su movimiento
+            for move in [(1,0), (-1,0), (0,-1), (0,1) ]:
 
-        #movimientos posibles
-        # diagonal no permitida
+                #print("checkpoint 1")
 
-        for n in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                #paso 2: calcular posicion actual
 
-        #for n in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+                #calcular posicion actual después de un movimiento
+                pos_actual = (n_actual.pos[0] + move[0], n_actual.pos[1] + move[1])
+                
+                #si la posicion actual esta fuera del mapa, no se agrega
+                if pos_actual[0] > (len(m) - 1) or pos_actual[0] < 0 or pos_actual[1] > (len(m[len(m) - 1]) - 1) or pos_actual[1] < 0:
+                    continue
+                
+                # depricated
+                #if m[pos_actual[0]][pos_actual[1]] != 0:
+                #    continue
 
-            # 0 = camino libre
-            # 1 = pared
-            # 2 = inicio
-            # 3 = fin
+                if m[pos_actual[0]][pos_actual[1]] == 0 or m[pos_actual[0]][pos_actual[1]] == 2 or m[pos_actual[0]][pos_actual[1]] == 3:
+                    #print("checkpoint 2")
 
-            node_pos = (node_c.pos[0] + n[0], node_c.pos[1] + n[1])
+                    #paso 3: calcular g, h y f y agregar a la lista de nodos o no
+
+                    g = n_actual.g + 1
+                    heu = 0
+                    if h == "manhattan":
+                        heu = manhattan(pos_actual[0], pos_actual[1], e[0], e[1])
+                    else:
+                        heu = Diagonal(pos_actual[0], pos_actual[1], e[0], e[1])
+                    f = g + heu
+
+                    #print("\n")
+                    #for n in nodes:
+                    #    print(n.pos)
 
 
+                    #print("checkpoint 3")
+
+                    # si la posicion actual ya esta en la lista de nodos, se actualiza el f si es menor
+                    i = 0
+                    while i < len(nodes):
+                        if nodes[i].pos == pos_actual:
+                            if nodes[i].f > f:
+                                nodes[i].f = f
+                            else:
+                                i += 1
+                        else:
+                            i += 1
+
+                    #print("checkpoint 4")
+
+                    #si la posicion actual no esta en la lista de nodos, se agrega
+                    new_node = Node(pos_actual, n_actual.pos)
+                    new_node.g = n_actual.g + 1
+                    new_node.setH(e, h)
+                    nodes.append(new_node)
+
+            # paso 4: seleccionar el nodo con menor f
 
 
-            if node_pos[0] > (len(maze) - 1) or node_pos[0] < 0 or node_pos[1] > (len(maze[len(maze) - 1]) - 1) or node_pos[1] < 0:
-                continue
+            print("checkpoint 5")
 
-            if maze[node_pos[0]][node_pos[1]] != 0:
-                continue
+            # problema con la actualización de lowest_f y n_actual
 
-            new_node = Node(node_pos, node_c)
+            #seleccionar el nodo con menor f
+            #lowest_f = [nodes[0].f, nodes[0].pos]
+            for node in nodes:
+                if node.f < lowest_f[0]:
+                    lowest_f = [node.f, node.pos]
 
-            sons.append(new_node)
 
-        #print("sons: ", sons)
-        #for e in sons: 
-            #print(e.pos, " , ", e.parent.pos, " , ", e.g, " , ", e.h, " , ", e.f)
-        print("*******************************************************")
-        for e in sons:
-            print(e.pos)
-
-        print("*******************************************************")
-
-        for son in sons:
+            #print("checkpoint 6")
+            #paso 5: actualizar el nodo actual
+            #n_actual = lowest_f[1]
+            for node in nodes:
+                if node.pos == lowest_f[1]:
+                    n_actual = node
             
-            for cs in _closed:
-                if son == cs:
-                    continue
+            print(n_actual.pos)
 
-            son.g = node_c.g + 1
-            if heuristic == "manhattan":
-                son.h = manhattan(son.pos[0], son.pos[1], node_f.pos[0], node_f.pos[1])
-            else:
-                son.h = Diagonal(son.pos[0], son.pos[1], node_f.pos[0], node_f.pos[1])
-            son.f = son.g + son.h
+        #paso 6: construir el camino
+        print(nodes)
+        path = []
+        current = n_actual
+        while current is not None:
+            path.append(current.pos)
+            current = current.parent
+        paths.append(path[::-1])
+    return paths
 
-            for op in _open:
-                if son == op and son.g > op.g:
-                    continue
 
-            _open.append(son)
+            
+
+
+
+
 
 # Prueba
 
 def main():
-   # maze = [
-   #     [3, 0, 0, 0, 3, 0],
-   #     [0, 1, 1, 1, 1, 0],
-   #     [0, 1, 0, 0, 1, 0],
-   #     [0, 1, 0, 0, 1, 0],
-   #     [0, 1, 0, 2, 1, 0],
-   #     [0, 0, 0, 0, 0, 0]
-   # ]
-#
-   # start = None
-   # end = None
-#
-#
-   # for i in range(len(maze)):
-   #     for j in range(len(maze[i])):
-   #         if maze[i][j] == 2:
-   #             start = (i, j)
-   #         if maze[i][j] == 3:
-   #             end = (i, j)
+    maze = [
+         [3, 0, 0, 0, 3, 0],
+         [0, 1, 1, 1, 1, 0],
+         [0, 1, 0, 0, 1, 0],
+         [0, 1, 0, 0, 1, 0],
+         [0, 1, 0, 2, 1, 0],
+         [0, 0, 0, 0, 0, 0]
+    ]
+    #start = None
+    #end = None
+    #for i in range(len(maze)):
+    #    for j in range(len(maze[i])):
+    #        if maze[i][j] == 2:
+    #            start = (i, j)
+    #        if maze[i][j] == 3:
+    #            end = (i, j)
 
 
-    maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-            
-    start = (0, 0)
-    end = (7, 6)
+    #maze = [[0, 0, 2, 0, 0, 0, 0, 0],
+    #        [0, 1, 1, 1, 0, 1, 1, 0],
+    #        [0, 1, 0, 0, 0, 0, 1, 0],
+    #        [0, 1, 0, 1, 1, 0, 1, 0],
+    #        [0, 1, 0, 0, 1, 0, 1, 3],
+    #        [0, 0, 0, 0, 0, 0, 1, 0],
+    #        [1, 1, 0, 1, 1, 0, 1, 0],
+    #        [0, 0, 0, 3, 0, 0, 0, 0]]
+    #start = (0, 2)
+    #end = (7, 4)
+
+    start = None
+    end = []
+
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == 2:
+                start = (i, j)
+            if maze[i][j] == 3:
+                end.append((i, j))
+
+    #formato de coordenadas y,x
 
     print(start)
     print(end)
 
-
-
-
-    path = astar(maze, start, end, "manhattan")
-
-    print("----------------------------------------------------")
+    path = astar_3(maze, start, end, "manhattan")
 
     print(path)
+
+    #path =  astar2(maze, start, end)
+
+
+
+
+    #path = astar(maze, start, end, "manhattan")
+#
+    #print("----------------------------------------------------")
+#
+    #print(path)
 
     #path = astar(maze, start, end, "diagonal")
 #
